f789a2cc81c35b8957cc19fc210e504b14c02b74
Sean Ransonette <sransonette@gmail.com>
2020-07-24 02:22:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,40 @@ class Song
     @@count += 1 
   end 
   
- 
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq 
+  end
+  
+  def self.artists 
+    @@artists.uniq 
+  end
+  
+  def self.genre_count
+    new_genre_count = {}
+    @@genres.each do |genre|
+      if new_genre_count[genre]
+        new_genre_count[genre] += 1 
+      else
+        new_genre_count[genre] = 1 
+      end
+      new_genre_count
+    end
+  end
+    
+  def self.artist_count
+    new_artist_count = {}
+    @@artists.each do |artist|
+      if new_artist_count[artist]
+        new_artist_count[artist] += 1 
+      else
+        new_artist_count[artist] = 1 
+      end
+    end
+  end
   
   
 end
